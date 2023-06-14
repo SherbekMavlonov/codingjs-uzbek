cf03fed84753958a220e78d7e231535b23166d22
Sherbek Mavlonov <mavlonovsherbek43@gmail.com>
2023-06-14 09:16:01 +0500

    INITIAL

---
+++ src/utility/tableHeader.js
@@ -0,0 +1,9 @@
+function tableHeader() {
+    return `<tr>
+                <th>Test → kutilmoqda</th>
+                <th style="padding: 2px 4px;">sizniki</th>
+                <th colspan="2" style="padding: 2px;">natija</th>
+            </tr>`;
+}
+
+module.exports = tableHeader;
